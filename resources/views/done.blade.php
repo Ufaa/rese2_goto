52ba0e2cfeec65a490d5916af35153cd060ba318
Ufaa <dhuwanee.no1@gmail.com>
2022-01-23 17:24:12 +0900

    20220123-2

--- resources/views/done.blade.php
+++ resources/views/done.blade.php
@@ -1,5 +1,43 @@
 @extends('layouts.default')
 <style>
+  body {
+    background-color: rgb(230, 230, 230);
+    position: relative;
+  }
+
+  .title {
+    font-size: 40px;
+    font-weight: bold;
+    color: #0033FF;
+    margin: 10px 0 0 70px;
+  }
+
+  .done-area {
+    background-color: white;
+    padding: 80px 100px;
+    border-radius: 3px;
+    box-shadow: 2px 2px 4px 1px gray;
+    position: absolute;
+    top: 30%;
+    left: 50%;
+    transform: translateY(-50%) translateX(-50%);
+    -webkit-transform: translateY(-50%) translateX(-50%);
+    text-align: center;
+  }
+
+  .done-area p {
+    font-weight: bold;
+  }
+
+  .back {
+    color: white;
+    background-color: #0033FF;
+    margin-top: 30px;
+    border-style: none;
+    border-radius: 5px;
+    padding: 5px 15px;
+  }
+
   th {
     background-color: #289ADC;
     color: white;
@@ -17,17 +55,14 @@
   }
 </style>
 
-@section('title', 'done.blade.php')
-
 @section('content')
+
 <body>
-
-  <h1>予約完了</h1>
-
-
-  <h4>ご予約ありがとうございます。</h4>
-  <button class="reset" type="button" onclick="location.href='/'">店舗一覧に戻る</button>
-
+  <div class="title"><a href="/">Rese</a>
+  </div>
+  <div class="done-area">
+    <p class="reservation-thanks">ご予約ありがとうございます。</p>
+    <button class="back" type="button" onclick="location.href='/'">戻る</button>
+  </div>
 </body>
 @endsection
-
