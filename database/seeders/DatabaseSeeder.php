423dab0396f2828b6c8b714d685e7e853c5a2b06
Ufaa <dhuwanee.no1@gmail.com>
2022-02-27 10:19:53 +0900

    seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(AreasTableSeeder::class);
-        //$this->call(GenresTableSeeder::class);
-        //$this->call(ShopsTableSeeder::class);
+        $this->call(AreasTableSeeder::class);
+        $this->call(GenresTableSeeder::class);
+        $this->call(ShopsTableSeeder::class);
         //$this->call(ReservationsTableSeeder::class);
         //$this->call(LikesTableSeeder::class);
         //$this->call(ReviewsTableSeeder::class);
